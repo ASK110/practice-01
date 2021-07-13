c658b7b04c390e18b68d6ac8be30f909cdbc7257
ASK110 <KATHIYAWAD.AA@GMAIL.COM>
2021-07-13 14:18:14 +0530

    json update

--- contents/conn.js
+++ contents/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://ASK110:1234@cluster0-shard-00-00.a2adr.mongodb.net:27017,cluster0-shard-00-01.a2adr.mongodb.net:27017,cluster0-shard-00-02.a2adr.mongodb.net:27017/test?replicaSet=atlas-c5cfkx-shard-0&ssl=true&authSource=admin",{seNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true})
+mongoose.connect("mongodb://ASK110:1234@cluster0-shard-00-00.a2adr.mongodb.net:27017,cluster0-shard-00-01.a2adr.mongodb.net:27017,cluster0-shard-00-02.a2adr.mongodb.net:27017/test?replicaSet=atlas-c5cfkx-shard-0&ssl=true&authSource=admin",{seNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true, useNewUrlParser: true})
 .then(()=>console.log("Database Connected..."))
 .catch((error)=>console.log(error))
